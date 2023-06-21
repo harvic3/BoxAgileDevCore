dfa9b90e97aad1aa88b19ad644b14f7551b4c7f6
Vickodev <harvic3@protonmail.com>
2023-06-21 11:23:14 -0500

    update: improvements in JsonUtil were made

--- BoxAgileDevCore/Controller/Utils/JsonUtils.cs
+++ BoxAgileDevCore/Controller/Utils/JsonUtils.cs
@@ -2,50 +2,50 @@
 
 namespace BoxAgileDevCore.Controller.Utils
 {
+  /// <summary>
+  /// Class for convert data in JSon to other object Type.
+  /// </summary>
+  public class JsonUtils
+  {
     /// <summary>
-    /// Class for convert data in JSon to other object Type.
+    /// Method for convert dynamic object to another object
     /// </summary>
-    public class JsonUtils
+    /// <typeparam name="T">Generic type to return</typeparam>
+    /// <param name="data">Object y Json</param>
+    /// <returns></returns>
+    public static T DeserializeObject<T>( dynamic data )
     {
-        /// <summary>
-        /// Method for convert dynamic object to another object
-        /// </summary>
-        /// <typeparam name="T">Generic type to return</typeparam>
-        /// <param name="data">Object y Json</param>
-        /// <returns></returns>
-        public T DeserializeObject<T>(dynamic data)
+      if ( data != null )
+      {
+        JsonSerializerSettings settings = new JsonSerializerSettings
         {
-            if (data != null)
-            {
-                JsonSerializerSettings settings = new JsonSerializerSettings
-                {
-                    NullValueHandling = NullValueHandling.Ignore,
-                    MissingMemberHandling = MissingMemberHandling.Ignore,
-                    Formatting = Formatting.Indented
-                };
-                return JsonConvert.DeserializeObject<T>(data.ToString(), settings);
-            }
-            return default(T);
-        }
-
-        /// <summary>
-        /// Method for serialize object
-        /// </summary>
-        /// <param name="obj">Object to serialize</param>
-        /// <returns></returns>
-        public string SerializeObject(object obj)
-        {
-            string result = JsonConvert.SerializeObject(obj, new JsonSerializerSettings
-            {
-                NullValueHandling = NullValueHandling.Ignore,
-                StringEscapeHandling = StringEscapeHandling.Default,
-                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-                DateFormatHandling = DateFormatHandling.IsoDateFormat,
-                FloatFormatHandling = FloatFormatHandling.DefaultValue,
-                Formatting = Formatting.Indented
-            });
-
-            return result;
-        }
+          NullValueHandling = NullValueHandling.Ignore,
+          MissingMemberHandling = MissingMemberHandling.Ignore,
+          Formatting = Formatting.Indented
+        };
+        return JsonConvert.DeserializeObject<T>( data.ToString(), settings );
+      }
+      return default( T );
     }
+
+    /// <summary>
+    /// Method for serialize object
+    /// </summary>
+    /// <param name="obj">Object to serialize</param>
+    /// <returns></returns>
+    public static string SerializeObject( object obj )
+    {
+      string result = JsonConvert.SerializeObject( obj, new JsonSerializerSettings
+      {
+        NullValueHandling = NullValueHandling.Ignore,
+        StringEscapeHandling = StringEscapeHandling.Default,
+        ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+        DateFormatHandling = DateFormatHandling.IsoDateFormat,
+        FloatFormatHandling = FloatFormatHandling.DefaultValue,
+        Formatting = Formatting.Indented
+      } );
+
+      return result;
+    }
+  }
 }
