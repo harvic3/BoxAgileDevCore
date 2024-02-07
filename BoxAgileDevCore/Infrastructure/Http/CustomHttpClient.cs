a1c26ee35157b779a1c329fb96821bfa73492466
Vickodev <harvic3@protonmail.com>
2024-02-06 23:14:22 -0500

    HttpClient was refactorized

--- BoxAgileDevCore/Infrastructure/Http/CustomHttpClient.cs
+++ BoxAgileDevCore/Infrastructure/Http/CustomHttpClient.cs
@@ -9,9 +9,11 @@ using System.Threading.Tasks;
 
 namespace BoxAgileDevCore.Infrastructure.Http
 {
-  public class CustomHttpClient
+  public sealed class CustomHttpClient : IDisposable
   {
     private readonly HttpClient httpClient;
+
+    private bool disposed = false;
 
     /// <summary>
     /// Constructor for CustomHttpClient
@@ -29,47 +31,59 @@ namespace BoxAgileDevCore.Infrastructure.Http
     /// <typeparam name="TError">Error data model for request</typeparam>
     /// <param name="url">Endpoint for request</param>
     /// <param name="method">HttpMethod for request</param>
-    /// <param name="content">HttpContent for request</param>
     /// <param name="headers">Headers for request</param>
-    /// <param name="jsonSerializerSettings">Json settings serializer</param>
+    /// <param name="content">HttpContent for request</param>
     /// <returns>(TResponse response, TError error, HttpStatusCode statusCode)</returns>
-    public async Task<(TResponse response, TError error, HttpStatusCode statusCode)> SendRequestAsync<TResponse, TError>( string url,
-                                                                                                                         HttpMethod method,
-                                                                                                                         HttpContent content,
-                                                                                                                         IDictionary<string, string> headers,
-                                                                                                                         JsonSerializerSettings? jsonSerializerSettings = null )
+    public async Task<(TResponse response, TError error, HttpStatusCode statusCode)> SendAsync<TResponse, TError>(
+      string url,
+      HttpMethod method,
+      IDictionary<string, string> headers,
+      HttpContent? content = null,
+      JsonSerializerSettings? jsonSerializerSettings = null
+      )
     {
-      var reqMessage = new HttpRequestMessage( method, url );
-      this.SetHeaders( headers, reqMessage );
+      var (reqMessage, cancellationToken) = this.CreateRequestMessage( url, method, headers, content );
 
-      reqMessage.Content = content;
-
-      var cancellationToken = new CancellationToken();
       HttpResponseMessage responseMessage = await this.httpClient.SendAsync( reqMessage, cancellationToken );
 
-      HttpStatusCode statusCode = responseMessage.StatusCode;
-      string stringResponse = await responseMessage.Content.ReadAsStringAsync();
+      return await this.DeserializeResponse<TResponse, TError>( responseMessage, jsonSerializerSettings );
+    }
 
-      try
+    public async Task<(TResponse response, TError[] errors, HttpStatusCode statusCode)> SendRetryAsync<TResponse, TError>(
+     string url,
+     HttpMethod method,
+     IDictionary<string, string> headers,
+     HttpContent? content,
+     int retries = 0,
+     int retryTimeount = 0,
+     JsonSerializerSettings? jsonSerializerSettings = null )
+    {
+      int iterartions = 0;
+      TError[] errors = new TError[retries == 0 ? 1 : retries];
+      HttpStatusCode statusCode;
+
+      do
       {
-        var serializerSettings = jsonSerializerSettings ??
-                                 new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
+        var (reqMessage, cancellationToken) = this.CreateRequestMessage( url, method, headers, content );
+
+        HttpResponseMessage responseMessage = await this.httpClient.SendAsync( reqMessage, cancellationToken );
 
         if ( responseMessage.IsSuccessStatusCode )
         {
-          TResponse response = JsonConvert.DeserializeObject<TResponse>( stringResponse, serializerSettings );
-          return (response, default( TError ), HttpStatusCode.OK);
+          var iteration = await this.DeserializeResponse<TResponse, TError>( responseMessage, jsonSerializerSettings );
+          return (iteration.response, errors, iteration.statusCode);
         }
         else
         {
-          TError response = JsonConvert.DeserializeObject<TError>( stringResponse, serializerSettings );
-          return (default( TResponse ), response, statusCode);
+          var iteration = await this.DeserializeResponse<TResponse, TError>( responseMessage, jsonSerializerSettings );
+          statusCode = iteration.statusCode;
+          errors[iterartions] = iteration.error;
+          iterartions++;
+          await Task.Delay( retryTimeount );
         }
-      }
-      catch ( Exception )
-      {
-        return (default( TResponse ), default( TError ), HttpStatusCode.InternalServerError);
-      }
+      } while ( iterartions <= retries );
+
+      return (default( TResponse ), errors, statusCode);
     }
 
     /// <summary>
@@ -83,5 +97,70 @@ namespace BoxAgileDevCore.Infrastructure.Http
       reqMessage.SetHeaders( headers );
     }
 
+    private (HttpRequestMessage reqMessage, CancellationToken cancellationToken) CreateRequestMessage(
+      string url, HttpMethod method, IDictionary<string, string> headers, HttpContent? request )
+    {
+      var reqMessage = new HttpRequestMessage( method, url );
+      this.SetHeaders( headers, reqMessage );
+
+      if ( request != null )
+        reqMessage.Content = request;
+
+      return (reqMessage, new CancellationToken());
+    }
+
+    private async Task<(TResponse response, TError error, HttpStatusCode statusCode)> DeserializeResponse<TResponse, TError>(
+      HttpResponseMessage responseMessage,
+      JsonSerializerSettings? jsonSerializerSettings
+    )
+    {
+      try
+      {
+        HttpStatusCode statusCode = responseMessage.StatusCode;
+        string stringResponse = await responseMessage.Content.ReadAsStringAsync();
+
+        if ( string.IsNullOrEmpty( stringResponse ) )
+        {
+          return (default( TResponse ), default( TError ), statusCode);
+        }
+
+        var serializerSettings = jsonSerializerSettings ??
+          new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore, DefaultValueHandling = DefaultValueHandling.Include };
+
+        if ( responseMessage.IsSuccessStatusCode )
+        {
+          TResponse response = JsonConvert.DeserializeObject<TResponse>( stringResponse, serializerSettings );
+          return (response, default( TError ), statusCode);
+        }
+        else
+        {
+          TError response = JsonConvert.DeserializeObject<TError>( stringResponse, serializerSettings );
+          return (default( TResponse ), response, statusCode);
+        }
+      }
+      catch ( Exception )
+      {
+        return (default( TResponse ), default( TError ), HttpStatusCode.InternalServerError);
+      }
+    }
+
+    public void Dispose()
+    {
+      Dispose( true );
+      GC.SuppressFinalize( this );
+    }
+
+    private void Dispose( bool disposing )
+    {
+      if ( disposed )
+        return;
+
+      if ( disposing )
+      {
+        httpClient.Dispose();
+      }
+
+      disposed = true;
+    }
   }
 }
